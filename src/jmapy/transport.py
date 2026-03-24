c9ce4149a572c23a22ecc68dad96bdab3a4e481a
Nathan Zilora <njz@cvm.com>
2026-03-24 15:56:21 +0000

    Added ORM

--- src/jmapy/transport.py
+++ src/jmapy/transport.py
@@ -1,63 +1,48 @@
 
-from collections.abc import Callable
-from typing import Concatenate, Self, overload
+import json
+from typing import Self
 
-session.request(
-    Foo.changes(account_id, since_state).then(
-        lambda resp: Foo.get(account_id, resp.created)
+from jmapy.models import ID
+from jmapy.orm import GettableData, MethodChain, Reference
+
+# session.request(
+#     Foo.changes(account_id, since_state).then(
+#         lambda resp: Foo.get(account_id, resp.created)
+#     )
+# )
+
+# session.request(
+#     Email.query(
+#         account_id,
+#         filter,
+#         sort,
+#         collapse_threads,
+#         position,
+#         limit,
+#         calculate_total
+#     ).then(
+#     lambda boxes: Email.get(account_id, boxes.ids).then(
+#     lambda emails: Thread.get(account_id, emails.list.all.thread_id).then(
+#     lambda threads: Email.get(account_id, threads.list.all.email_ids)
+#     )))
+# )
+
+class User(GettableData): ...
+
+class Foo(GettableData):
+    attr = Reference[Self, ID]()
+
+class Bar(GettableData): ...
+
+def exec[T, *Ts](test: MethodChain[T, *Ts]) -> None:
+    print(json.dumps(test.calls, indent=2))
+
+
+if __name__ == "__main__":
+    exec(
+    User.get("12", ["223"]).then(
+        lambda usr: Foo.get(usr.account_id, ["123"]).then(
+        lambda foo: Bar.get(foo.account_id, foo.list.all.attr).then(
+        Foo.get("231", ["34"])
+        )))
     )
-)
-
-session.request(
-    Email.query(
-        account_id,
-        filter,
-        sort,
-        collapse_threads,
-        position,
-        limit,
-        calculate_total
-    ).then(
-    lambda boxes: Email.get(account_id, boxes.ids).then(
-    lambda emails: Thread.get(account_id, emails.list.all.thread_id).then(
-    lambda threads: Email.get(account_id, threads.list.all.email_ids)
-    )))
-)
-
-class Test[S, *Ts]:
-
-    @overload
-    def then[T, *Rs](self, cmd: "Callable[[S], Test[T, *Rs]]") -> "Test[S, T, *Rs, *Ts]": ...
-
-    @overload
-    def then[T, *Rs](self, cmd: "Test[T, *Rs]") -> "Test[S, T, *Rs]": ...
-
-    def then[T, *Rs](self, cmd: "Callable[[S], Test[T, *Rs]] | Test[T, *Rs]") -> "Test[S, T, *Rs, *Ts] | Test[S, T, *Rs]": ...
-
-class User:
-
-    @classmethod
-    def get(cls) -> Test[Self]: ...
-
-class Foo:
-
-    @classmethod
-    def get(cls) -> Test[Self]: ...
-
-
-class Bar:
-
-    @classmethod
-    def get(cls) -> Test[Self]: ...
-
-
-def exec[T, *Ts](test: Test[T, *Ts]) -> tuple[T, *Ts]: ...
-
-resp = exec(
-    User.get().then(
-        lambda usr: Foo.get().then(
-        lambda foo: Bar.get().then(
-        Foo.get()
-        ))
-    ),
-) 
